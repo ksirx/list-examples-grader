c4092ecf56b1d6c49873f8a32fa8c8bc132500c7
Xavier N Navarro <cs15lsp23cv@ieng6-203.ucsd.edu>
2023-05-24 18:34:17 -0700

    new

--- grade.sh
+++ grade.sh
@@ -8,8 +8,17 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission == false ]]
+then
+	exit	
+else
+	cp -r *.java student-submission
+    	echo "check exit code"
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
+echo "exiting if statements"
+
+# a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
